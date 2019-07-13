729f2117408451172923ce4296a02f076fd0b109
Frank Crooks <fcrooks1@gmail.com>
2019-07-12 17:07:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,145 @@
+function genuine() {
+  var x = document.getElementById("genuineText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function thoughtful() {
+  var x = document.getElementById("thoughtfulText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function empathetic() {
+  var x = document.getElementById("empatheticText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function education() {
+  var x = document.getElementById("educationText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function philanthropy() {
+  var x = document.getElementById("philanthropyText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function emergingTechnology() {
+  var x = document.getElementById("emergingTechnologyText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function publicBenefitCorporations() {
+  var x = document.getElementById("publicBenefitCorporationsText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function background() {
+  var x = document.getElementById("backgroundText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function identify() {
+  var x = document.getElementById("identifyText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function reflect() {
+  var x = document.getElementById("reflectText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function act() {
+  var x = document.getElementById("actText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function conclusion() {
+  var x = document.getElementById("conclusionText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
